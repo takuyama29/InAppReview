bd9273795bcba418c8ffe8801ad9063b6e3ae0ba
Taku Yamada <tmn.sby001@gmail.com>
2017-09-05 15:23:01 +0900

    Add review button

--- InAppReview/ViewController.swift
+++ InAppReview/ViewController.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func requestReviewButton(_ sender: UIButton) {
+        // レビューページへ遷移
+        if #available(iOS 10.3, *) {
+            SKStoreReviewController.requestReview()
+        }
+            // iOS 10.3未満の処理
+        else if let url = URL(string: "itms-apps://itunes.apple.com/app/id1274048262?action=write-review") {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:])
+            }
+            else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
